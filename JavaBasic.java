a66039f30d7aa30df740acd1ff09f7536b34e146
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-18 06:51:40 +0530

    All Rights

---
+++ JavaBasic.java
@@ -0,0 +1,9 @@
+public class JavaBasic {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
+
+// 'ln' => Line ka space
