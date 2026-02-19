0f2c0e0a21c928fd1eb39a5608bd12786f002f0c
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-19 06:41:17 +0530

    Yes

---
+++ Variables in Java/Memory.java
@@ -0,0 +1,10 @@
+public class Memory {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        System.out.print(a);
+        System.out.print("b");
+        System.out.println(b);
+    }
+}
