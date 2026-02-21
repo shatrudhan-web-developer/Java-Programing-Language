969139f8691ed986f59b4acfeadc80a5fe7c8c09
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-21 22:03:25 +0530

    Yes

--- Input in Java/Input.java
+++ Input in Java/Input.java
@@ -4,7 +4,13 @@ public class Input {
     public static void main(String args []) {
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.next();
-        System.out.println(input);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        int number = sc.nextInt();
+        System.out.println(number);
     }
 }
