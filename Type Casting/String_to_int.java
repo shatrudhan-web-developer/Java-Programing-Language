661b3401b73f41eaa2a1ca3598a53fd744dd0034
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-23 11:56:01 +0530

    Yes

---
+++ Type Casting/String_to_int.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class String_to_int {
+    public static void main(String args[]) {
+
+        char ch = 'a';
+        char ch2 = 'b';
+
+        int number = ch;
+        int number2 = ch2;
+
+        System.out.println(number);
+    }
+}
