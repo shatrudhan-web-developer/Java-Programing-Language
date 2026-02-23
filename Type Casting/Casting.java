bbb5f499b0df42a0bb4ad9f99ea02ca2e00f2853
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-23 11:41:31 +0530

    Yes

---
+++ Type Casting/Casting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Casting {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        float number = 99.999f;
+        int number2 = (int) number;
+
+        System.out.println(number);
+    }
+}
