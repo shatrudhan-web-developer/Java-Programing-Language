42bb3267f2498e2a92267204d2afc7b989be120e
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-23 11:18:12 +0530

    Yes

---
+++ Input in Java/Area_of_Curcle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Area_of_Curcle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        Float rad = sc.nextFloat();
+        Float Area = 3.14f * rad * rad;
+
+        System.out.println(Area);
+    }
+}
