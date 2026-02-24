b117e7affa4ca54fcd6f426043c54263ad7d0140
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-24 15:25:13 +0530

    Yes

---
+++ Arithmetic Oprtators/Binary Operators/Binary/Binary.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Binary {
+    public static void main(String args[]) {
+
+        int A = 10;
+        int B = 5;
+
+        System.out.println("Sum = " + (A + B));
+        System.out.println("Subtraction = " + (A - B));
+        System.out.println("Multiplication = " + (A * B));
+        System.out.println("Modulo(Remider) = " + (A % B));
+    }
+}
