72009f23f5ce388c5f73c0fb83b76fdbc3fef924
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-24 14:04:03 +0530

    Yes

--- Variables in Java/Promotion2.java
+++ Variables in Java/Promotion2.java
@@ -1,14 +1,26 @@
+// import java.util.*;
+
+// public class Promotion2 {
+//     public static void main(String args[]) {
+
+//         int a = 10;
+//         float b = 20.25f;
+//         long c = 25;
+//         double d = 30;
+        
+//         double ans = a + b + c + d;
+//         System.out.println(ans);
+//     }
+// }
+
+            //Wrong & Right
+
 import java.util.*;
 
 public class Promotion2 {
     public static void main(String args[]) {
 
-        int a = 10;
-        float b = 20.25f;
-        long c = 25;
-        double d = 30;
-        
-        int ans = a + b + c + d;
-        System.out.println(ans);
+        byte b = 5;
+        b = b*2;
     }
 }
