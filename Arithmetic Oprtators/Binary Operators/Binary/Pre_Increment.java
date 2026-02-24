e6969c761c4bf22c0c8f0c247a43d86284d2589f
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-24 15:29:55 +0530

    Yes

---
+++ Arithmetic Oprtators/Binary Operators/Binary/Pre_Increment.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Pre_Increment {
+    public static void main(String args[]) {
+
+            // ===> ++a
+        
+        int a = 10;
+        int b = ++a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
