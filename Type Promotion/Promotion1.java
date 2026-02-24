b317e4ccb038e502ce25b8f96b8409d4c6bf411d
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-24 13:35:07 +0530

    Yes

---
+++ Type Promotion/Promotion1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Promotion1 {
+    public static void main(String args[]) {
+
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println((int) (b));
+    }
+}
