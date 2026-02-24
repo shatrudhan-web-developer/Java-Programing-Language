6a30af496b2ab0f1a65bdf4cf5419661059c2c71
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-02-24 13:44:18 +0530

    Yes

--- Type Promotion/Promotion1.java
+++ Type Promotion/Promotion1.java
@@ -1,11 +1,30 @@
+// import java.util.*;
+
+// public class Promotion1 {
+//     public static void main(String args[]) {
+
+//         char a = 'a';
+//         char b = 'b';
+
+//         System.out.println((int) (b));
+//         System.out.println((int) (a));
+
+//         System.out.println(b - a);
+//         System.out.println(a);
+//     }
+// }
+
 import java.util.*;
 
 public class Promotion1 {
     public static void main(String args[]) {
 
-        char a = 'a';
-        char b = 'b';
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
 
-        System.out.println((int) (b));
+        System.out.println(bt);
+
     }
 }
