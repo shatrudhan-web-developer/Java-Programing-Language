c5e5c26e52a50d406cd4db703756d0317585a26b
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-08 18:23:37 +0530

    Yes

---
+++ Logical Operators/Logical_OR.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Logical_OR {
+    public static void main(String arg[]) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a < 15 || b > a);
+    }
+}
