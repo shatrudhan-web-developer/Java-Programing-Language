2c5fa48d63af0e23eaf855fb2157e6b33afd9224
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-08 17:57:18 +0530

    Yes

---
+++ Relational Operators/Relational_Operators.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Relational_Operators {
+    public static void main(String arg[]) {
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println(a == b);
+    }
+}
