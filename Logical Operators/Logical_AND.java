f59297d955ca0d0622609e456e12789a669fc1b6
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-08 18:17:27 +0530

    Yes

---
+++ Logical Operators/Logical_AND.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Logical_AND {
+    public static void main(String arg[]) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > 5 && b < 10);
+    }
+}
--- Relational Operators/Relational_Operators.java
+++ Relational Operators/Relational_Operators.java
@@ -3,9 +3,34 @@ import java.util.*;
 public class Relational_Operators {
     public static void main(String arg[]) {
 
-        int a = 10;
+        // int a = 10;
+        // int b = 10;
+
+        // System.out.println(a == b);
+
+        // int a = 10;
+        // int b = 5;
+
+        // System.out.println(a != b);
+
+        // int a = 15;
+        // int b = 10;
+
+        // System.out.println(a > b);
+
+        // int a = 50;
+        // int b = 10;
+
+        // System.out.println(a < b);
+
+        // int a = 11;
+        // int b = 10;
+
+        // System.out.println(a >= b);
+
+        int a = 5;
         int b = 10;
 
-        System.out.println(a == b);
+        System.out.println(a <= b);
     }
 }
