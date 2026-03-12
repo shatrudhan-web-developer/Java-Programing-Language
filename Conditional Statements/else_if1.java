173bc901854f261fdc60d7d55276350fd481665b
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-12 11:58:45 +0530

    Push

--- Conditional Statements/else_if1.java
+++ Conditional Statements/else_if1.java
@@ -13,7 +13,10 @@ public class else_if1 {
             tax = 0;
         }
         else if(income >= 500000 && income < 1000000) {
-            tax = income * 0.2;
+            tax = (int) (income * 0.2);
+        }
+        else {
+            tax = (int)(income * 0.3);
         }
     }
 }
