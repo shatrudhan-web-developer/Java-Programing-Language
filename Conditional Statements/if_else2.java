9c544e77ed6e4e12eaa1a932fe5a68353b749c26
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-12 11:14:20 +0530

    Push

---
+++ Conditional Statements/if_else2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class if_else2 {
+    public static void main(String args[]) {
+
+        // Print of largest of 2 numbers
+
+        int a = 1, b = 5;
+
+        if(a >= b) {
+            System.out.println("Largest number of a");
+        }
+        else {
+            System.out.println("Largest number of b");
+        }
+    }
+}
