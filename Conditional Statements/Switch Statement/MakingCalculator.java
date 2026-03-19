454ba3e1afde7fc4025713134fd7c4843eb32d94
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-19 18:35:43 +0530

    Yes

---
+++ Conditional Statements/Switch Statement/MakingCalculator.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MakingCalculator {
+    public static void main(String args[]) {
+
+        // Makinng normal Calculator
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number = ");
+        int number1 = sc.nextInt();
+
+        System.out.println("Enter second number = ");
+        int number2 = sc.nextInt();
+
+        char Operator = sc.next().charAt(0)
+
+        switch(Operator) {
+            case '+' : System.out.println("Addition = " + (number1 + number2));
+                        break;
+
+            case '-' : System.out.println("Subtraction = " + (number1 - number2));
+                        break;
+
+            case '*' : System.out.println("Multiplication = " + (number1 * number2));
+                        break;
+
+            case '/' : System.out.println("Deviding = " + (number1 / number2));
+                        break;
+
+            case '%' : System.out.println("Modelas = " + (number1 % number2));
+                        break;
+
+            default : System.out.println("Not Support");
+        }
+    }
+}
