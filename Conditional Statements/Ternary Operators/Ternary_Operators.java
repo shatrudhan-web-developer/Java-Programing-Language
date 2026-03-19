b36a7194e9809c7351b272e3a6dd3b0106661ab8
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-19 17:35:37 +0530

    Right

--- Conditional Statements/Ternary Operators/Ternary_Operators.java
+++ Conditional Statements/Ternary Operators/Ternary_Operators.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Ternary_Operators {
     public static void main(String args[]) {
-
         Scanner sc = new Scanner(System.in);
-        int Number = sc.nextInt();
-        String Type = (Number / 2 == 0)? "Odd" : "Even";
 
-        System.out.println(Type);
+        System.out.print("Enter number = ");
+        int number = sc.nextInt();
 
+        String Type = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("This number is " + Type);
     }
 }
