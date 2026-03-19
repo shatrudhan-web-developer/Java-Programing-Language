bcac19e6e0bb10a24ea9fd89c20dfa5db1e22aba
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-19 23:06:55 +0530

    Right

---
+++ Loop (Flow Control)/2. For Loop/ForLoop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ForLoop {
+    public static void main(String args[]) {
+        for(int i = 1; i <= 1000; i++) {
+            System.out.println("Noor Ji Miss You");
+        }
+    }
+}
