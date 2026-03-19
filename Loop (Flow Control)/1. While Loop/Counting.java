89e5c40ff26f25de19a8d10b95ae747f9a55705f
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-03-19 19:10:16 +0530

    Yes

---
+++ Loop (Flow Control)/1. While Loop/Counting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Counting {
+    public static void main(String args[]) {
+
+        // Print number from 1 to 10
+
+        int Counting = 1;
+
+        System.out.println("=====Counting from 1 to 10=====");
+        while(Counting <= 10) {
+            System.out.println(Counting);
+            Counting++;
+        }
+    }
+}
--- Loop (Flow Control)/1. While Loop/WhileLoop.java
+++ Loop (Flow Control)/1. While Loop/WhileLoop.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class WhileLoop {
     public static void main(String args[]) {
 
-        int Counter = 0;
+        int Counter = 1;
 
         while(Counter <= 10) {
             System.out.println("Noor Meri Jaan 😘");
+            Counter++;
         }
+        System.out.println("Printed 10X");
     }
 }
